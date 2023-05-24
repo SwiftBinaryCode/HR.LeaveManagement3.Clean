4ef96c49411668d0589bdc670227ac94a41fe183
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2023-05-24 21:52:27 +0200

    Api project set up

--- HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -1,12 +1,9 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveTYpe;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using HR.LeaveManagement.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.MappingProfiles
 {
@@ -16,6 +13,9 @@ namespace HR.LeaveManagement.Application.MappingProfiles
         {
             CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
             CreateMap<LeaveType, LeaveTypeDetailsDto>();
+            CreateMap<CreateLeaveTypeCommand, LeaveType>();
+            CreateMap<UpdateLeaveTypeCommand, LeaveType>();
+
         }
     }
 }
